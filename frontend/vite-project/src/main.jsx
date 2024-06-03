04bcd52ee686c3db6f2936e98344bd377d680e9f
Ethan-code-1 <ebo4dq@virginia.edu>
2024-06-03 17:05:39 -0400

    Setup navbar and roots folder with base components.

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+import Home from './roots/Home';
+import Navbar from './components/Navbar.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navbar></Navbar>,
+  },
+  {
+    path: '/recipes',
+    element: <Navbar></Navbar>,
+  },
+  {
+    path: '/recipeView',
+    element: <Navbar></Navbar>,
+  },
+  {
+    path: '/myRecipes',
+    element: <Navbar></Navbar>,
+  },
+  {
+    path: '/admin',
+    element: <Navbar></Navbar>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
