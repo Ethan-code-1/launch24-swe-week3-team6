869b42fa9a77d160ccd863407f3a12ec320372dc
Ethan-code-1 <ebo4dq@virginia.edu>
2024-06-04 14:56:06 -0400

    Added logo to login page.

--- frontend/vite-project/src/components/Login.jsx
+++ frontend/vite-project/src/components/Login.jsx
@@ -7,13 +7,13 @@ const Login = () => {
       <div className="login-overlay"></div>
       <div className="right-overlay"></div> 
       <form className="login-form">
-        <h1 className="company-name">Flavor Fusion</h1>
+        <div className="logo-container">
+          <h1 className="company-name">Flavor Fusion</h1>
+          <img src="/websitelogo.png" alt="Recipe App Logo" className="logo-image" />
+        </div>
         <p className="login-subhead">Find just the recipe you have been looking for.</p>
-
         <hr className="homeHr" />
-
         <h1 className="login-title">Login</h1>
-
         <input type="text" className="login-input" placeholder="Username" />
         <input type="password" className="login-input" placeholder="Password" />
         <button className="login-button">Submit</button>
