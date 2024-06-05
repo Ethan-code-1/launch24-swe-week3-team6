3536a064d36ef5ff98dd50f72b84d0944ac2c4fc
Rayna Bhattacharyya <raynabhattacharyya@gmail.com>
2024-06-05 14:32:03 -0700

    posting reviews

--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -13,7 +13,35 @@ router.get("/:id", async (req, res) => {
         const rid = req.params.id
         const recDoc = await getDoc(doc(db, 'recipes', rid));
         const rec = recDoc.data();
-        res.status(200).json(rec);
+
+        let reviews = await getDocs(collection(db, 'recipes', rid, 'reviews'));
+        // console.log(reviews);
+        if (reviews) {
+            reviews = await Promise.all(reviews.docs.map(async (review) => {
+                let revData = review.data();
+                const userDoc = await getDoc(doc(db, 'users', revData.uid));
+                const userData = userDoc.data();
+                revData['user'] = userData;
+                // console.log(revData)
+                return revData;
+            }));
+            // console.log
+        } else {
+            reviews = [];
+        }
+
+        res.status(200).json({'rec': rec, 'revs': reviews});
+    } catch (e) {
+        res.status(500).send({ error: e.message });
+    }
+})
+
+router.put("/review/:id", async (req, res) => {
+    try {
+        const rid = req.params.id;
+        console.log(req.body);
+        await addDoc(collection(db, 'recipes', rid, 'reviews'), req.body)
+        res.status(200).send("Added Successfully!");
     } catch (e) {
         res.status(500).send({ error: e.message });
     }
