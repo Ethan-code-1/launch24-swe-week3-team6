a935a461c1415c2bb6bde64a3b81d068fb4416f3
Ethan-code-1 <ebo4dq@virginia.edu>
2024-06-06 16:41:26 -0400

    Updated accept functionality on backend for admin accept.

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,9 @@ import myRecipesRouter from './routes/myRecipes.js';
 import recipeRouter from './routes/recipe.js';
 import authRouter from './routes/auth.js';
 import chatRouter from './routes/chat.js';
+import adminRouter from './routes/admin.js';
+
+dotenv.config(); // Ensure dotenv is configured before using environment variables
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -16,23 +19,18 @@ const app_key = process.env.APP_KEY;
 
 app.use(express.json());
 app.use(cors());
-dotenv.config();
 
 // Initialize Routes
 app.use('/api/recipes', recipePage);
-
+app.use('/home', homeRouter);
+app.use('/myRecipes', myRecipesRouter);
+app.use('/recipe', recipeRouter);
+app.use('/api/auth', authRouter);
+app.use('/chat', chatRouter);
+app.use('/admin', adminRouter);
 
 // access point: https://api.edamam.com/api/recipes/v2
-
-// placeholding the url
-const url =
-  "https://api.edamam.com/api/recipes/v2?type=public&app_id=${app_id}app_key=${app_key}";
-
-app.use("/home", homeRouter);
-app.use("/myRecipes", myRecipesRouter);
-app.use("/recipe", recipeRouter);
-app.use("/api/auth", authRouter);
-app.use("/chat", chatRouter);
+const url = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${app_id}&app_key=${app_key}`;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
