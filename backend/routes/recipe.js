14a500ed6d4262511d36f2b14bdd0da79d126681
Rayna Bhattacharyya <raynabhattacharyya@gmail.com>
2024-06-06 10:58:59 -0700

    replies to reviews work

--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -22,7 +22,12 @@ router.get("/:id", async (req, res) => {
                 const userDoc = await getDoc(doc(db, 'users', revData.uid));
                 const userData = userDoc.data();
                 revData['user'] = userData;
-                // console.log(revData)
+                revData['id'] = review.id;
+                revData['replies'] = await Promise.all(revData.replies.map(async (rep) => {
+                    const uDoc = await getDoc(doc(db, 'users', rep.uid));
+                    rep['user'] = uDoc.data();
+                    return rep;
+                }))
                 return revData;
             }));
             // console.log
@@ -41,6 +46,22 @@ router.put("/review/:id", async (req, res) => {
         const rid = req.params.id;
         console.log(req.body);
         await addDoc(collection(db, 'recipes', rid, 'reviews'), req.body)
+        res.status(200).send("Added Successfully!");
+    } catch (e) {
+        res.status(500).send({ error: e.message });
+    }
+})
+
+router.put("/comment/:id", async (req, res) => {
+    try {
+        const rid = req.params.id;
+        const data = req.body;
+        console.log(data);
+        const revDocRef = doc(db, 'recipes', rid, 'reviews', data.revId);
+        const revDoc = await getDoc(revDocRef)
+        const replies = revDoc.data()['replies'];
+        replies.push({uid: data.uid, content: data.reply, time: data.timestamp});
+        await updateDoc(revDocRef, {replies: replies});
         res.status(200).send("Added Successfully!");
     } catch (e) {
         res.status(500).send({ error: e.message });
